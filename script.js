134e36cfece9b0a403e871e3ba9a77d2057f4bdf
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-07-23 20:11:34 -0300

    feat: annotation e Inference no typescript

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
+"use strict";
 // function somar(a: number, b: number) {
 //   return a + b;
 // }
 // console.log(somar(10, 50));
-
 // // ANNOTATION
-
 // const produto: string = "Livro";
 // const preco: number = 100;
 // console.log(produto, preco);
-
 // const carro: {
 //   marca: string;
 //   preco: number;
@@ -18,36 +16,22 @@
 //   preco: 1000,
 //   portas: 5,
 // };
-
 // console.log(carro);
-
 // function normalizarTextos(texto: string) {
 //   return texto.trim().toLowerCase();
 // }
-
 // console.log(normalizarTextos("TESTE2"));
-
 const input = document.querySelector("input");
 const total = localStorage.getItem("total");
-if (input && total) {
-  input.value = total;
-  calcularGanho(+input.value);
-}
-
-function calcularGanho(value: number) {
-  const p = document.querySelector("p");
-  if (p) {
+input.value = total;
+calcularGanho(input.value);
+function calcularGanho(value) {
+    const p = document.querySelector("p");
     p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
-  }
 }
-
 function totalMudou() {
-  if (input) {
-    localStorage.setItem("total", input.value);
-    calcularGanho(+input.value);
-  }
+    const value = +input.value;
+    localStorage.setItem("total", value);
+    calcularGanho(value);
 }
-
-if (input) {
-  input.addEventListener("keyup", totalMudou);
-}
+input.addEventListener("keyup", totalMudou);
